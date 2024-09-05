45561234bd71244c76d26aec448f29ed5fb70836
SHAHIL444 <110395532+Shahilali5@users.noreply.github.com>
2024-09-05 13:44:48 +0530

    Update kang.py

--- kang.py
+++ kang.py
@@ -2,50 +2,49 @@ import hashlib
 import os
 import math
 import urllib.request as urllib
-
 from io import BytesIO
 from PIL import Image
 from pyrogram import Client
-
 import telegram
 import logging
 import json
-
 from typing import Optional, List
 from telegram import ParseMode, InlineKeyboardMarkup, InlineKeyboardButton
-from telegram import TelegramError
-from telegram import Update, Bot
+from telegram import TelegramError, Update, Bot
 from telegram.ext import CommandHandler, run_async, Updater, Handler, InlineQueryHandler
 from telegram.utils.helpers import escape_markdown
-
 from telegram import Message, Chat, MessageEntity, InlineQueryResultArticle
 from os import path
 
 logging.basicConfig(level=logging.INFO, format="%(asctime)s - %(name)s - %(levelname)s - %(message)s")
 logger = logging.getLogger()
 
-def getConfig(name: str):
-    return os.environ[name]
-
-try:
-    TOKEN = getConfig('TOKEN')
-except KeyError as e:
-    LOGGER.error("TOKEN env variables missing! Exiting now")
-    exit(1)
+# Direct token added here
+TOKEN = '7314483805:AAHRJNDWLblogkdkwqITcgFh6ZSjrwl8tmg'
 
 updater = telegram.ext.Updater(token=TOKEN)
 bot = updater.bot
 dispatcher = updater.dispatcher
 
+# Admin chat IDs added here
+ADMIN_CHAT_IDS = [7165556607, 5985044373]
+
 START_TEXT = """
 Hey! I'm {}, Welcome To Devil Sticker Bot\n\nThis Bot Created By @Shahil44 & @D3VIL_BOY)
 """.format(dispatcher.bot.first_name)
 
+# Notify admins about new users
+def notify_admins(user):
+    for admin_id in ADMIN_CHAT_IDS:
+        bot.send_message(chat_id=admin_id, text=f"New user {user.full_name} ({user.id}) started using the bot!")
+
 @run_async
 def start(bot: Bot, update: Update):
     if update.effective_chat.type == "private":
+        # Notify the admin on new user registration
+        notify_admins(update.effective_user)
+        
         update.effective_message.reply_text(START_TEXT, parse_mode=ParseMode.MARKDOWN)
-
 
 @run_async
 def kang(bot: Bot, update: Update, args: List[str]):
@@ -163,124 +162,12 @@ def kang(bot: Bot, update: Update, args: List[str]):
         os.remove("kangsticker.png")
 
 
-@run_async
-def kangurl(bot: Bot, update: Update, args: List[str]):
-    msg = update.effective_message
-    user = update.effective_user
-    packnum = 0
-    packname = "a" + str(user.id) + "_by_"+bot.username
-    packname_found = 0
-    max_stickers = 120
-    while packname_found == 0:
-        try:
-            stickerset = bot.get_sticker_set(packname)
-            if len(stickerset.stickers) >= max_stickers:
-                    packnum += 1
-                    packname = "a" + str(packnum) + "_" + str(user.id) + "_by_"+bot.username
-            else:
-                packname_found = 1
-        except TelegramError as e:
-            if e.message == "Stickerset_invalid":
-                packname_found = 1
-    if args:
-        kangsticker = "kangsticker.png"
-        try:
-            try:
-                urlemoji = msg.text.split(" ")
-                png_sticker = urlemoji[1] 
-                sticker_emoji = urlemoji[2]
-            except IndexError:
-                sticker_emoji = "🤔"
-            urllib.urlretrieve(png_sticker, kangsticker)
-            im = Image.open(kangsticker)
-            if (im.width and im.height) < 512:
-                size1 = im.width
-                size2 = im.height
-                if im.width > im.height:
-                    scale = 512/size1
-                    size1new = 512
-                    size2new = size2 * scale
-                else:
-                    scale = 512/size2
-                    size1new = size1 * scale
-                    size2new = 512
-                size1new = math.floor(size1new)
-                size2new = math.floor(size2new)
-                sizenew = (size1new, size2new)
-                im = im.resize(sizenew)
-            else:
-                maxsize = (512, 512)
-                im.thumbnail(maxsize)
-            im.save(kangsticker, "PNG")
-            msg.reply_photo(photo=open('kangsticker.png', 'rb'))
-            bot.add_sticker_to_set(user_id=user.id, name=packname,
-                                    png_sticker=open('kangsticker.png', 'rb'), emojis=sticker_emoji)
-            msg.reply_text(f"Sticker successfully added to [pack](t.me/addstickers/{packname})" +
-                            f"\nEmoji is: {sticker_emoji}", parse_mode=ParseMode.MARKDOWN)
-        except OSError as e:
-            msg.reply_text("I can only kang images, m8.")
-            print(e)
-            return
-        except TelegramError as e:
-            if e.message == "Sticker_png_dimensions":
-                im.save(kangsticker, "PNG")
-                bot.add_sticker_to_set(user_id=user.id, name=packname,
-                                        png_sticker=open('kangsticker.png', 'rb'), emojis=sticker_emoji)
-                msg.reply_text(f"Sticker successfully added to [pack](t.me/addstickers/{packname})" +
-                                f"\nEmoji is: {sticker_emoji}", parse_mode=ParseMode.MARKDOWN)
-            elif e.message == "Stickers_too_much":
-                msg.reply_text("Max packsize reached. Press F to pay respects.")
-            elif e.message == "Stickerset_invalid":
-                makepack_internal(msg, user, open('kangsticker.png', 'rb'), sticker_emoji, bot, packname, packnum)
-            print(e)
-    else:
-        msg.reply_text("Please provide a URL to kang!")
-    if os.path.isfile("kangsticker.png"):
-        os.remove("kangsticker.png")
+# Existing handlers remain
+kang_handler = CommandHandler('kang', kang, pass_args=True)
+start_handler = CommandHandler('start', start)
 
+dispatcher.add_handler(kang_handler)
+dispatcher.add_handler(start_handler)
 
-@run_async
-def makepack_internal(msg: Message, user: telegram.User, png_sticker, emoji, bot: Bot, packname: str, packnum: int):
-    name = user.first_name + "'s kang pack"
-    if packnum > 0:
-        name += f" v{packnum}"
-    success = False
-    try:
-        success = bot.create_new_sticker_set(user.id, packname, name, png_sticker=png_sticker, emojis=emoji)
-    except TelegramError as e:
-        msg.reply_text(f"Failed to create sticker pack. Maybe try kang later.")
-        print(e)
-    if success:
-        msg.reply_text(f"Sticker pack created successfully!\n\nHere is your [pack](t.me/addstickers/{packname})",
-                        parse_mode=ParseMode.MARKDOWN)
-    else:
-        msg.reply_text(f"Failed to create sticker pack.")
-
-
-@run_async
-def help_command(bot: Bot, update: Update):
-    update.effective_message.reply_text(
-        "/kang - Reply to a sticker, image, or file to add it to your sticker pack.\n"
-        "/kangurl [url] [emoji] - Use a direct image URL to add a sticker.\n\n"
-        "By default, sticker emoji will be 🤔 unless you provide one. For example:\n"
-        "/kangurl https://i.imgur.com/abc123.png 😎\n"
-    )
-
-
-def main():
-    start_handler = CommandHandler("start", start)
-    kang_handler = CommandHandler("kang", kang, pass_args=True)
-    kangurl_handler = CommandHandler("kangurl", kangurl, pass_args=True)
-    help_handler = CommandHandler("help", help_command)
-
-    dispatcher.add_handler(start_handler)
-    dispatcher.add_handler(kang_handler)
-    dispatcher.add_handler(kangurl_handler)
-    dispatcher.add_handler(help_handler)
-
-    updater.start_polling()
-    updater.idle()
-
-
-if __name__ == '__main__':
-    main()
+updater.start_polling(timeout=15, read_latency=4)
+updater.idle()
